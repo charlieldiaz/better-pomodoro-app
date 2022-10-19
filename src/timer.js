a22df036c818af552b8e308377e5ea88454fbefb
Carlos Diaz <carlosdiaz@Carloss-MacBook-Pro.local>
2022-10-18 22:31:49 -0700

    update on my project

--- src/timer.js
+++ src/timer.js
@@ -1,10 +1,17 @@
+import { getQueriesForElement } from '@testing-library/react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
+const red = "#FF5733"
 function Timer() {
     return (
         <div>
-            <CircularProgressbar value={60} text={'60%'} styles={buildStyles({ rotation, st })} />
+            <CircularProgressbar value={60} text={'60%'} styles={buildStyles({
+                textColor: '#fff',
+                pathColor: red,
+                trailColor: getQueriesForElement,
+            })} />
+
         </div>
     )
 }
